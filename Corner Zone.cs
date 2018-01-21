f70966125be1feea5db15140e02ba765f81e6922
David Brungardt <34960989+brungardtdb@users.noreply.github.com>
2018-01-20 17:03:37 -0700

    Initial commit

---
+++ Corner Zone.cs
@@ -0,0 +1,156 @@
+﻿using System;
+
+
+class Corner_Zone
+{
+    private double _zoneSize; // instance variable
+
+    // property to get and set the zoneSize instance variable
+    // (zone size called out by design)
+    public double ZoneSize
+    {
+        get
+        {
+            return _zoneSize;
+        }
+        set
+        {
+            _zoneSize = value;
+        }
+    }
+
+    private double _verticalZoneSize; // instance variable
+
+    // property to get and set the zoneSize instance variable
+    // (zone size called out by design)
+    public double VerticalZoneSize
+    {
+        get
+        {
+            return _verticalZoneSize;
+        }
+        set
+        {
+            _verticalZoneSize = value;
+        }
+    }
+
+    private double _startFinishDim; // instance variable
+
+    // property to get and set the startFinishDim instance variable
+    // (Start / Finish dimension for roof sheeting)
+    public double StartFinishDim
+    {
+        get
+        {
+            return _startFinishDim;
+        }
+        set
+        {
+            _startFinishDim = value;
+        }
+    }
+
+    private double _cornerZoneActual; // instance variable
+
+    // property to get and set the cornerZoneActual instance variable
+    // (actual size of corner zone)
+    public double CornerZoneActual
+    {
+        get
+        {
+            return _cornerZoneActual;
+        }
+        set
+        {
+            _cornerZoneActual = _zoneSize - _startFinishDim;
+        }
+    }
+
+
+    private int _clipsPerPurlin; // instance variable
+
+    // property to get and set the clipsPerPurlin instance variable
+    // (amount of clips to go on each run of purlins)
+    public int ClipsPerPurlin
+    {
+        get
+        {
+            return _clipsPerPurlin;
+        }
+        set
+        {
+            _clipsPerPurlin = value;
+        }
+    }
+
+    private int _totalPerimeterClips; // instance variable
+
+    // property to get and set the clipsPerPurlin instance variable
+    // (amount of clips to go on each run of purlins)
+    public int TotalPerimeterClips
+    {
+        get
+        {
+            return _totalPerimeterClips;
+        }
+        set
+        {
+            _totalPerimeterClips = value;
+        }
+    }
+
+    private int _panelsInCornerZone; // instance variable
+
+    // property to get and set the panelsInCornerZoe instance variable
+    // (amount of panels in the corner zone)
+    public int PanelsInCornerZone
+    {
+        get
+        {
+            return _panelsInCornerZone;
+        }
+        set
+        {
+            _panelsInCornerZone = value;
+        }
+    }
+
+
+    private bool _correctStartFinish = false;
+
+    // property to get and set the correctPanel instance variable
+    // (property to terminate panel qty loop)
+    public bool CorrectStartFinish
+    {
+        get
+        {
+            return _correctStartFinish;
+        }
+        set
+        {
+            _correctStartFinish = value;
+        }
+    }
+
+
+    private bool _correctPurlinSpace = false;
+
+    // property to get and set the correctPanel instance variable
+    // (property to terminate panel qty loop)
+    public bool CorrectPurlinSpace
+    {
+        get
+        {
+            return _correctPurlinSpace;
+        }
+        set
+        {
+            _correctPurlinSpace = value;
+        }
+    }
+
+
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+
+class Program
+{
+    
+    
+    static void Main()
+    {
+
+        // Declaration Section
+
+        Corner_Zone CornerZone1 = new Corner_Zone();    
+
+        // Ask the user to enter a size (in inches) for the corner zone called out by design.
+
+        Console.Write($"Please enter the horizontal zone size called out by design (inches): ");
+        var zoneSize = Console.ReadLine();
+        CornerZone1.ZoneSize = Convert.ToDouble(zoneSize);
+        Console.WriteLine($"Horizontal zone size is: {CornerZone1.ZoneSize} inches");
+
+        // Ask the user to enter a start/finish dimension for the sheeting plan (in inches)
+        // give them an error message if the dimension smaller or larger than our standards.
+        do
+        {
+            Console.Write($"Please enter the start dimension for the sheeting plan (inches): ");
+            var startFinishDim = Console.ReadLine();
+            CornerZone1.StartFinishDim = Convert.ToDouble(startFinishDim);
+
+            if (CornerZone1.StartFinishDim < 12.125)
+            {
+                CornerZone1.CorrectStartFinish = false;
+                Console.WriteLine($"Please enter a start dimension (inches) between 12.125 and 24.125");
+            }
+            else if (CornerZone1.StartFinishDim > 24.125)
+            {
+                CornerZone1.CorrectStartFinish = false;
+                Console.WriteLine($"Please enter a start dimension (inches) between 12.125 and 24.125");
+            }
+            else
+            {
+                CornerZone1.CorrectStartFinish = true;
+               
+            }
+            
+        }
+        while (CornerZone1.CorrectStartFinish == false);
+
+        Console.WriteLine($"Start / Finish dimension is {CornerZone1.StartFinishDim} inches");
+
+        // Based on on the corner zone dimension called out by the engineer, and the Start/Finish
+        // dimension provided from the sheeting plan, calculate the number of perimeter clips per
+        // purlin run on the roof sheeting plan.
+
+
+        var cornerZoneActual = CornerZone1.ZoneSize - CornerZone1.StartFinishDim;
+
+        CornerZone1.ClipsPerPurlin = Convert.ToInt32((cornerZoneActual / 24) + 1);
+        Console.WriteLine($"Number of clips per purlin run: { CornerZone1.ClipsPerPurlin}");
+
+
+        // Ask user for the vertical zone size called out by design (inches)
+        Console.Write($"Please enter the vertical zone size called out by design (inches): ");
+        var verticalZoneSize = Console.ReadLine();
+        CornerZone1.VerticalZoneSize = Convert.ToDouble(verticalZoneSize);
+        Console.WriteLine($"Vertical zone size is: {CornerZone1.VerticalZoneSize} inches");
+
+
+        // Ask user for number of purlins and calculate the total number of perimeter clips.
+
+        double totalPurlinInches = 0; // initialize sum of total purlin runs (inches)
+        int purlinRunCounter = 0; // initialize the # of purlin runs entered so far
+
+        // Prompt for input and read purlin runs (inches) from the user.
+        Console.WriteLine($"Please enter the first purlin space (inches): ");
+        double purlinRun = double.Parse(Console.ReadLine());
+
+        do
+        {
+            totalPurlinInches = totalPurlinInches + purlinRun; // Add purlin run to the total
+            purlinRunCounter = purlinRunCounter + 1; // increment counter
+
+            // Prompt for input and read purlin runs (inches) from the user.
+            Console.WriteLine($"Please enter the next purlin space (inches): ");
+            purlinRun = double.Parse(Console.ReadLine());
+
+            if (totalPurlinInches < CornerZone1.VerticalZoneSize)
+            {
+                CornerZone1.CorrectPurlinSpace = false;
+            }
+            else
+            {
+                CornerZone1.CorrectPurlinSpace = true;
+            }
+        }
+        while (CornerZone1.CorrectPurlinSpace == false); // Terminate the loop when the purlin runs (inches) exceeds the vertial zone size. 
+
+        // if the user entered at least one purlin run
+        if (purlinRunCounter != 0)
+        {
+            // Calculate the total number of perimeter clips in the corner zone
+            var totalPerimeterClips = purlinRunCounter * CornerZone1.ClipsPerPurlin;
+            CornerZone1.TotalPerimeterClips = totalPerimeterClips;
+            Console.WriteLine($" Total Perimeter Clips: {CornerZone1.TotalPerimeterClips} ");
+        }
+        else
+        {
+            Console.WriteLine("No purlin runs entered");
+        }
+
+    }
+}
+
+    
+
